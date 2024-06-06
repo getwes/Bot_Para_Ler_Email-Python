b60d8844320f9aca865f9f0b9f5cd5053b1416a3
gitwes <wesleyfreire1707@gmail.com>
2024-06-05 22:09:34 -0300

    Criando layout do projeto

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
 # Qual biblioteca python PysimpleGUI
 # pip install pysimplegui
+
+#Fazer toda configuração PysimpleGUI
+#import PySimpleGUI as sg
+
+#sg.popup('ok')
+
+import PySimpleGUI as sg
+
+sg.theme('reddit')
+
+layout = [
+    
+]
