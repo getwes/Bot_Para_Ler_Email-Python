e7a5a56e0fe925914868b05ebd3011c3b98cfb88
gitwes <wesleyfreire1707@gmail.com>
2024-06-19 22:23:04 -0300

    Ajustando botão de salvar

--- app.py
+++ app.py
@@ -10,11 +10,18 @@ import PySimpleGUI as sg
 
 sg.theme('reddit')
 
-layout = [
+#ou janela_principal
+janela_principal = [
     [sg.Text('E-mail'),sg.Input(key='email')],
-    [sg.Text('senha'),sg.Input(key='senha', password_char='*')],
-    [sg.FolderBrowse('Escolher pasta anexos' , targuet='input_anexos'),sg.Input(key='input_anexos')],
-    [sg.FolderBrowse('Escolher pasta planilha' , targuet='input_planilha'),sg.Input(key='input_planilha')],
-    [sg.button('Salvar')]  
+    [sg.Text('senha'),sg.Input(key='senha',password_char='*')],
+    [sg.FolderBrowse('Escolher pasta anexos',target='input_anexos'),sg.Input(key='input_anexos')],
+    [sg.FolderBrowse('Escolher pasta planilha',target='input_planilha'),sg.Input(key='input_planilha')],
+    [sg.Button('Salvar')]  
 ]
+#aqui em Layout=janela_principal
+janela = sg.Window('principal',layout=janela_principal)
 
+while True:
+    event, values = janela.read()
+    if event == sg.WIN_CLOSED:
+        break
